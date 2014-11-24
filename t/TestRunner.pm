7ccbe261202a46f51ebd1be64c03faf8f9e802c4
skliew <skliew@gmail.com>
2014-11-24 13:49:43 +0800

    Add test for overridden methods.

--- t/TestRunner.pm
+++ t/TestRunner.pm
@@ -5,40 +5,50 @@ use Test::More;
 use Carp qw(croak);
 
 BEGIN {
-    use_ok( 'Test::HTTP::VCR' ) || print "Bail out!\n";
+    use_ok('Test::HTTP::VCR') || print "Bail out!\n";
 }
 
 sub run {
     my $httpclientname = shift or croak "Must pass in httpclient";
 
-    SKIP: {
+  SKIP: {
         eval "require $httpclientname";
         skip "$httpclientname not installed", 2 if $@;
 
-        my ($fh, $tempfile) = tempfile();
+        my ( $fh, $tempfile ) = tempfile();
 
-        my $vcr = Test::HTTP::VCR->new(
-            $tempfile,
-            {HTTPCLIENT => $httpclientname}
-        );
+        my $vcr =
+          Test::HTTP::VCR->new( $tempfile, { HTTPCLIENT => $httpclientname } );
         my $httpclient = $httpclientname->new;
 
+        my $request_method_name = $httpclientname . '::request';
+        my $request_method_code = \&$request_method_name;
+
         my $response;
-        $vcr->record(sub {
+        $vcr->record(
+            sub {
+                cmp_ok( $request_method_code, '!=', \&$request_method_name,
+                    "$request_method_name is changed" );
                 $response = $httpclient->get('https://google.com');
-            });
+            }
+        );
+
+        cmp_ok( $request_method_code, '==', \&$request_method_name,
+            "$request_method_name is restored" );
 
         my $response_playbank;
-        $vcr->play(sub {
+        $vcr->play(
+            sub {
                 $response_playbank = $httpclient->get('https://google.com');
-            });
+            }
+        );
 
-        is_deeply($response_playbank, $response, 'Played response is correct');
+        is_deeply( $response_playbank, $response,
+            'Played response is correct' );
 
         unlink $tempfile;
     }
     done_testing();
 }
-
 
 1;
